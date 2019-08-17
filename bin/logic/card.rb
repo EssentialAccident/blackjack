99253f8800e9672b6d8a0017a5da0c4e6a509bbb
Lino Oropeza <linooropeza@gmail.com>
2019-08-17 13:10:41 -0400

    Added the possibility of face donw cards. Changed card letters to full words

--- bin/logic/card.rb
+++ bin/logic/card.rb
@@ -1,16 +1,37 @@
 # This card holds the logic for a single playing card
 class Card
   SUITS = %w[hearts diamonds clubs spades].freeze
-  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].freeze
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'].freeze
 
   attr_reader :suit, :value
 
-  def initialize(suit, value)
+  def initialize(suit, value, face_up = true)
     @suit = suit
     @value = value
+    @face_up = face_up
+  end
+
+  def face_up?
+    @face_up
+  end
+
+  def face_up
+    @face_up = true
+  end
+
+  def face_down
+    @face_up = false
+  end
+
+  def flip
+    @face_up = !@face_up
   end
 
   def to_s
-    "#{@value} of #{@suit}"
+    if face_up
+      "#{@value} of #{@suit}"
+    else
+      'Down Card'
+    end
   end
 end
