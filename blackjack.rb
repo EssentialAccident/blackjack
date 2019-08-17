334ce2a1a8e22b2ad1773762735ca1732b8ae4d9
Lino Oropeza <linooropeza@gmail.com>
2019-08-17 10:31:26 -0400

    Adding the newer dependencies to the blackjack file. And, movign the game window to the graphics folder

--- blackjack.rb
+++ blackjack.rb
@@ -2,7 +2,9 @@
 require 'gosu'
 
 # Local dependencies
-require_relative 'bin/game_window'
+require_relative 'bin/graphics/game_window'
+require_relative 'bin/logic/card'
+require_relative 'bin/logic/deck'
 
 # Main class for the Black Jack game
 class BlackJack
