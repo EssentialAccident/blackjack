320d1088e3d080735f9db65f98351dbebc2c55c0
Lino Oropeza <linooropeza@gmail.com>
2019-08-17 10:57:52 -0400

    Added the cards_left and shuffle methods to the deck class

--- bin/logic/deck.rb
+++ bin/logic/deck.rb
@@ -1,6 +1,41 @@
 # This class holds one or more decks of 52 playing cards
 class Deck
   def initialize(number_of_decks = 1)
-    @num_decks = number_of_decks
+    @deck = create_deck number_of_decks
+  end
+
+  def show_deck
+    @deck.each do |card|
+      puts card.to_s
+    end
+  end
+
+  def cards_left
+    @deck.lenght
+  end
+
+  # The deck will be shuffled between 10 to 20 times
+  # The number of times that the deck will be shuffled
+  # is picked randomly
+  def shuffle
+    shuffles = rand(10..20)
+    puts "Deck shuffled #{shuffles} times"
+    shuffles.times do
+      @deck.shuffle!
+    end
+  end
+
+  private
+
+  def create_deck(num_decks)
+    deck = []
+    (0...num_decks).each do
+      Card::SUITS.each do |suit|
+        Card::VALUES.each do |value|
+          deck.append(Card.new(suit, value))
+        end
+      end
+    end
+    deck
   end
 end
