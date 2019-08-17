303d68f686d7235ea9dc474dfc375bad568833db
Lino Oropeza <linooropeza@gmail.com>
2019-08-17 13:11:14 -0400

    Added all the new dependencies to the black jack file

--- blackjack.rb
+++ blackjack.rb
@@ -5,12 +5,16 @@ require 'gosu'
 require_relative 'bin/graphics/game_window'
 require_relative 'bin/logic/card'
 require_relative 'bin/logic/deck'
+require_relative 'bin/logic/player'
+require_relative 'bin/logic/human_player'
+require_relative 'bin/logic/ai_dealer'
+require_relative 'bin/logic/game'
 
 # Main class for the Black Jack game
 class BlackJack
   def initialize
-    @window = GameWindow.new
-    @deck = Deck.new
+    # @window = GameWindow.new
+    @game = Game.new
   end
 
   def run
