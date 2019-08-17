fa19df8b341e31f8d82e328aeab9a10df4b22529
Lino Oropeza <linooropeza@gmail.com>
2019-08-17 11:58:02 -0400

    Changing number of decks and adding the draw method

--- bin/logic/deck.rb
+++ bin/logic/deck.rb
@@ -1,17 +1,29 @@
-# This class holds one or more decks of 52 playing cards
+# This class holds a deck of playing cards
 class Deck
+  attr_reader :number_of_decks
+
+  # It creates a number_of_decks decks of cards
+  # If number_of_decks is 0 it creates an empty deck
+  # This can be good to create discard decks in the future
   def initialize(number_of_decks = 1)
-    @deck = create_deck number_of_decks
+    @number_of_decks = number_of_decks
+    @deck = if number_of_decks > 0
+              create_deck @number_of_decks
+            else
+              []
+            end
   end
 
-  def show_deck
+  def show
     @deck.each do |card|
       puts card.to_s
     end
+    puts
+    puts "#{cards_left} cards on deck"
   end
 
   def cards_left
-    @deck.lenght
+    @deck.length
   end
 
   # The deck will be shuffled between 10 to 20 times
@@ -23,6 +35,14 @@ class Deck
     shuffles.times do
       @deck.shuffle!
     end
+  end
+
+  # It returns the top card from the deck
+  # This card will not longer be on the deck
+  def draw
+    card = @deck[0]
+    @deck.delete(card)
+    card
   end
 
   private
