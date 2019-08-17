1e9f573729c56267b09970797e01d98cd0b871d0
Lino Oropeza <linooropeza@gmail.com>
2019-08-17 10:56:06 -0400

    Added attribute readers to suit and value

--- bin/logic/card.rb
+++ bin/logic/card.rb
@@ -3,8 +3,14 @@ class Card
   SUITS = %w[hearts diamonds clubs spades].freeze
   VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].freeze
 
+  attr_reader :suit, :value
+
   def initialize(suit, value)
     @suit = suit
     @value = value
+  end
+
+  def to_s
+    "#{@value} of #{@suit}"
   end
 end
